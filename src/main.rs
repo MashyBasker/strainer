1cf6e2c78136e8e6c435774a4bf1ba4c450344ee
Maharshi Basu <basumaharshi10@gmail.com>
2025-09-24 23:36:01 +0530

    loggo
    Signed-off-by: Maharshi Basu <basumaharshi10@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{error::Error, io::{BufReader, BufRead}, process::{Command, Stdio}};
+use std::{error::Error, io::{BufRead, BufReader}, process::{Command, Stdio}};
+use std::io::{self, Write};
+use std::fs::File;
 use regex::Regex;
 use std::env;
 
@@ -22,10 +24,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let program_args: Vec<String> = args.collect();
+    let mut program_args: Vec<String> = args.collect();
 
-    let mut child = Command::new(program)
-        .args(program_args)
+    // Check for --out= argument and extract output path if present
+    let out_path = if let Some(pos) = program_args.iter().position(|arg| arg.starts_with("--out=")) {
+        let path = program_args[pos].trim_start_matches("--out=").to_string();
+        program_args.remove(pos);
+        Some(path)
+    } else {
+        None
+    };
+
+    let mut child = Command::new(&program)
+        .args(&program_args)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
@@ -34,23 +45,37 @@ fn main() -> Result<(), Box<dyn Error>> {
     let stderr = BufReader::new(child.stderr.take().unwrap());
 
     let re = Regex::new(&pattern)?;
+    
+    let out: Box<dyn Write + Send> = if let Some(path) = out_path {
+        Box::new(File::create(path)?)
+    } else {
+        Box::new(io::stdout())
+    };
+    
+    let writer = std::sync::Arc::new(std::sync::Mutex::new(out));
 
     let re1 = re.clone();
+    let w1 = writer.clone();
     let t1 = std::thread::spawn(move || {
         for line in stdout.lines() {
             if let Ok(line) = line {
                 if re1.is_match(&line) {
-                    println!("{line}");
+                    let mut out = w1.lock().unwrap();
+                    writeln!(out, "{line}").ok();
                 }
             }
         }
     });
 
     let re2 = re.clone();
+    let w2 = writer.clone();
     let t2 = std::thread::spawn(move || {
-        for line in stderr.lines().flatten() {
-            if re2.is_match(&line) {
-                println!("{line}");
+        for line in stderr.lines() {
+            if let Ok(line) = line {
+                if re2.is_match(&line) {
+                    let mut out = w2.lock().unwrap();
+                    writeln!(out, "{line}").ok();
+                }
             }
         }
     });
